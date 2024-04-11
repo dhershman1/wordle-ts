13cb285fbb430498305e2163a4f627811aa918ba
dhershman1 <dustinh17@gmail.com>
2024-04-11 00:27:30 -0400

    Exctracted constant

--- src/components/__tests__/WordleBoard.spec.ts
+++ src/components/__tests__/WordleBoard.spec.ts
@@ -3,7 +3,7 @@ import { describe, test, expect } from 'vitest'
 import { mount } from '@vue/test-utils'
 import WordleBoard from '../WordleBoard.vue'
 
-import { VICTORY_MESSAGE } from '@/settings'
+import { VICTORY_MESSAGE, DEFEAT_MESSAGE } from '@/settings'
 
 describe('WordleBoard', () => {
   test('A Victory message appears when the user makes a guess that matches the word of the day', async () => {
@@ -23,7 +23,7 @@ describe('WordleBoard', () => {
     await guessInput.setValue('WRONG')
     await guessInput.trigger('keydown.enter')
 
-    expect(wrapper.text()).toContain("Better luck next time!")
+    expect(wrapper.text()).toContain(DEFEAT_MESSAGE)
   })
   test.todo('no end of game message appears if the user has not yet made a guess')
 })
