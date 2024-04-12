b20357b63c700907fcceb7e1dffdee6aa5cff465
dhershman1 <dustinh17@gmail.com>
2024-04-12 00:49:53 -0400

    Select Random word

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,10 @@
 <script setup lang="ts">
 import WordleBoard from '@/components/WordleBoard.vue'
+import englishWords from '@/englishWordsWith5Letters.json'
+
+const randomWord = englishWords[Math.floor(Math.random() * englishWords.length)]
 </script>
 
 <template>
-    <wordle-board word-of-the-day="TESTS" />
+    <wordle-board :word-of-the-day="randomWord" />
 </template>
